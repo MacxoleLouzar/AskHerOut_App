1f94f50a539da2034ae8bfcad7ec5cff02ae7454
macxolelouzaar <macxolelouzaar@gmail.com>
2026-07-24 20:57:18 +0200

    Add gh-pages deploy config

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AskHerOut_App/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
